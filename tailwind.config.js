466a5393cdd5cde1117f2bb62b1022604d66c033
rickychan0611 <ric0611@gmail.com>
2024-05-01 14:12:20 -0700

    move files to src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { hairlineWidth } = require('nativewind/theme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: ['./**/*.{ts,tsx}', './**/*.{ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
     borderRadius: {
